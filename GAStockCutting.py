3107ca0da3ebdba05efa87602033480535883bdd
radeeb bashir <radeebbashir@gmail.com>
2018-10-08 23:49:51 -0500

    added the text file and added reading from file

--- GAStockCutting.py
+++ GAStockCutting.py
@@ -1,33 +1,13 @@
-'''
-Missouri State University, Fall 2018
-Ken Vollmar
-Genetic algorithm for stock cutting
-Assumptions:
--- Stock and pieces are rectangles with integer coordinates, and 
-        edges are oriented strictly "north-south" and "east-west." This 
-        simplifies determining whether two pieces intersect.
--- Assume that the "cut" has zero width, so that two edges may be
-        coincident and still valid. Example:  y1 --- yA:::::yB --------y2
--- The size of the stock, and the size and position of all pieces, are
-        specified by the upper-left and lower-right corner coordinates.
-
-
-tkinter documentation:
-http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/index.html
-https://www.c-sharpcorner.com/blogs/basics-for-displaying-image-in-tkinter-python
-https://tkdocs.com/
-'''
-
 import time  # for pause during graphic display
 import random
 import sys
 
 # import tkFont  # http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/fonts.html
-random.seed(time.time())  # Initialize internal state of the random number generator.
+random.seed(time.time()) # Initialize internal state of the random number generator.
 
-NUMBER_OF_PIECES = 6  # HARDCODED  
-STOCK_WIDTH = 800  # HARDCODED  Width of stock
-STOCK_HEIGHT = 400  # HARDCODED  Height of stock =
+#NUMBER_OF_PIECES = 6  # HARDCODED
+#STOCK_WIDTH = 800  # HARDCODED  Width of stock
+#STOCK_HEIGHT = 400  # HARDCODED  Height of stock =
 NUMBER_OF_GENERATIONS = 10  # HARDCODED Number of generations of evolution
 POPULATION_SIZE = 10  # HARDCODED Number of individuals in population
 
@@ -82,33 +62,31 @@ def makeRectObj(w, h, x1, y1, c):
             "x1": x1, "y1": y1,
             "x2": x1 + w, "y2": y1 + h}  # Return a dictionary object
 
+
 def switchRects(p1, rect1, p2, rect2):
     swtch = p1[rect1]
     p1[rect1] = p2[rect2]
     p2[rect2] = swtch
 
 
-# Use tkinter to display stock and pieces
-from tkinter import *
-
-root = Tk()
-canvas = Canvas(root, width=STOCK_WIDTH, height=STOCK_HEIGHT, bg='khaki')
-canvas.pack()
-
 # Read data from a file if a file is given on the command line.
 # Open the file and read it into the list "content"
 if (len(sys.argv) > 1):  # A command-line argument exists; assume it is an input filename
     filename = sys.argv[1]
-    '''
-    # This section not genuinely indented, but necessary for comment
-    else: # Prompt for input filename
-            filename = input("\n\n\tPlease type an input data file name: ")
-    '''
+# This section not genuinely indented, but necessary for comment
+else:  # Prompt for input filename
+    #filename = input("\n\n\tPlease type an input data file name: ")
+    filename = "Test1Data.txt"
+
     try:
         with open(filename) as f:
             content = f.readlines()
     except FileNotFoundError:
         sys.exit('Could not find file ' + filename)
+
+    STOCK_WIDTH = int(content[0])
+    STOCK_HEIGHT = int(content[1])
+    NUMBER_OF_PIECES = 5
 
     # Data in file is expected to be, on separate lines as shown:
     #       Width of stock
@@ -126,18 +104,34 @@ if (len(sys.argv) > 1):  # A command-line argument exists; assume it is an input
             print(str(line[j]))
         # print()
 
+
+
+# Use tkinter to display stock and pieces
+from tkinter import *
+
+root = Tk()
+canvas = Canvas(root, width=STOCK_WIDTH, height=STOCK_HEIGHT, bg='khaki')
+canvas.pack()
+
+
+
 ''' Initialize and display POPULATION_SIZE number of pieces.
 TBD -- Initially, create all pieces of _size_ 200x200, 
 at _positions_ TBD.  
 TBD -- this places the piece within the stock (as opposed to putting
 at least the UL corner but not necessarily the LR corner in stock)
 '''
+
+
 population = [0 for i in range(POPULATION_SIZE)]
 for indiv_count in range(POPULATION_SIZE):
     individual = [0 for j in range(NUMBER_OF_PIECES)]
-    for piece_count in range(NUMBER_OF_PIECES):
-        w = 200  # TBD HARDCODED
-        h = 200  # TBD HARDCODED
+
+    #for piece_count in range(NUMBER_OF_PIECES):
+    for piece_count in range(0, 5):
+        line = content[piece_count+3].split()
+        w = int(line[0])  # TBD HARDCODED
+        h = int(line[1])  # TBD HARDCODED
         c = piece_colors[piece_count]  # TBD -- need more colors when more pieces
         x1 = random.randint(0, STOCK_WIDTH - w)  # piece is within stock
         y1 = random.randint(0, STOCK_HEIGHT - h)  # piece is within stock
@@ -154,8 +148,8 @@ for indiv_count in range(POPULATION_SIZE):
             canvas.update()
             time.sleep(0.02)  # HARDCODED
 
-        print("individual  is ", individual)
-        print()
+        # print("individual  is ", individual)
+        # print()
 
     # The population is an array of individual objects
     population[indiv_count] = individual
@@ -170,7 +164,6 @@ for indiv_count in range(POPULATION_SIZE):
 
 
 
-
 '''
 This is the main GA loop, performing the evolutionary sequence of
 operations: Evaluation, Selection, Crossover, Mutation.
@@ -178,8 +171,9 @@ Remember:
         A POPULATION is a set of POPULATION_SIZE individuals.
         An INDIVIDUAL is a set of PIECE_COUNT pieces.
 '''
+
 for looper in range(NUMBER_OF_GENERATIONS):
-    print("---------------------Generation: ", looper)
+
     # EVALUATE ALL INDIVIDUALS
     fitness = [0 for i in range(POPULATION_SIZE)]
     fit_count = 0
@@ -188,7 +182,7 @@ for looper in range(NUMBER_OF_GENERATIONS):
         sum_fitness = 0
         for piece in indiv:
             x1 = piece.get("x1")
-            #print(x1)
+            print(x1)
             x2 = piece.get("x2")
             y1 = piece.get("y1")
             y2 = piece.get("y2")
@@ -197,15 +191,16 @@ for looper in range(NUMBER_OF_GENERATIONS):
                     if compGrid[m][n] == 0:
                         compGrid[m][n] = 1
                         sum_fitness += 1
-        fitness[fit_count] = (1/((STOCK_HEIGHT * STOCK_WIDTH) - sum_fitness))*10**6 #to have better representation while selection
-        fitness[fit_count] = round(fitness[fit_count], 3)
+        fitness[fit_count] = ((STOCK_HEIGHT * STOCK_WIDTH) - sum_fitness)
         fit_count += 1
-        #print("fitness is:", fitness)
-    print("fitness is:", fitness)
-    print()
+        print(fitness)
 
+    # Display all pieces in their new position.
+    # In general, display the fittest individual of this generation.
+    # In this demo, display only the first individual.
+    # Clear the display by re-drawing the background with no elements
     canvas.create_rectangle(0, 0, STOCK_WIDTH, STOCK_HEIGHT, fill='khaki')
-    fittest_index = fitness.index(max(fitness))
+    fittest_index = fitness.index(min(fitness))
     display_individual = population[fittest_index]  # display this individual, which is a list of dictionary
 
     print("fittest index is ", fittest_index)
@@ -224,43 +219,35 @@ for looper in range(NUMBER_OF_GENERATIONS):
     canvas.update()
     time.sleep(1)  # HARDCODED TIME -- pause briefly between generations
 
-    # SELECT INDIVIDUALS FOR REPRODUCTION IN THE NEXT GENERATIONCROSSOVER OPERATION FOR INDIVIDUALS
-    #fitnesses = [chromosome.fitness for chromosome in population]
-    total_fitness = float(sum(fitness))
-    #print("total fitness:" , total_fitness)
+    # SELECT INDIVIDUALS FOR REPRODUCTION IN THE NEXT GENERATION
+    p1 = random.randint(0, 9)
+    p2 = random.randint(0, 9)
+    while (p1 == p2):
+        p2 = random.randint(0, 9)
+    ind_1 = population[p1]
+    ind_2 = population[p2]
+    # CROSSOVER OPERATION FOR INDIVIDUALS
+    switchRects(ind_1, random.randint(0, 5), ind_2, random.randint(0, 5))
 
-    relative_fitness = [f / total_fitness for f in fitness]  # totals to 1
-    # Generate probability intervals for each individual
-    probList = [sum(relative_fitness[:i + 1]) for i in range(len(relative_fitness))]
-    # Draw new population
-    r = random.uniform(0, 1)
-    for (i, individual) in enumerate(population):
-        if r <= probList[i]:
-            p1 = individual
-            #print("p1 is: ", p1)
-    for (i, individual) in enumerate(population):
-        if r <= probList[i]:
-            p2 = individual
-            #print("p2 is: ", p2)
-
-    #--------------------------------------------------------------------------------
-    #--------------------------------------------------------------------------------
-
-    #CROSSOVER IDNDIVIDUALS
-
-    switchRects(p1, random.randint(0,5), p2, random.randint(0,5))
+    p1 = random.randint(0, 9)
+    p2 = random.randint(0, 9)
+    while (p1 == p2):
+        p2 = random.randint(0, 9)
+    ind_1 = population[p1]
+    ind_2 = population[p2]
+    switchRects(ind_1, random.randint(0, 5), ind_2, random.randint(0, 5))
 
     # MUTATION OPERATION FOR INDIVIDUALS
     # In general, select with some randomness "several" individuals upon which
     # to perform mutation of "some" (one or more) characteristics.
     # TBD This demo is hardcoded to change only the first characteristic of the first individual.
-    mutating_individual = population[0]  # mutating_individual is a list of dictionary
+    mutating_individual = population[random.randint(0, 9)]  # mutating_individual is a list of dictionary
+    # print()
+    # print("mutating indiv is ", mutating_individual)
     print()
-    print("mutating indiv is ", mutating_individual)
-    print()
-    mutating_characteristic = mutating_individual[0]
-    print(" mutating_characteristic is ", mutating_characteristic)
-    print()
+    mutating_characteristic = mutating_individual[random.randint(0, 5)]
+    # print(" mutating_characteristic is ", mutating_characteristic)
+    # print()
 
     prev_value = mutating_characteristic.get("x1")
     new_value = prev_value + 5  # Mutate by incrementing
@@ -268,11 +255,5 @@ for looper in range(NUMBER_OF_GENERATIONS):
     prev_value = mutating_characteristic.get("x2")
     new_value = prev_value + 5  # Mutate by incrementing
     mutating_characteristic["x2"] = new_value
-
-    # Display all pieces in their new position.
-    # In general, display the fittest individual of this generation.
-    # In this demo, display only the first individual.
-    # Clear the display by re-drawing the background with no elements
-
 
 mainloop()  # Graphics loop -- This statement follows all other statements
